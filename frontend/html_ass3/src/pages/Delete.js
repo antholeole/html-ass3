ed9fca4b09bd6316ce38242ee51f1f75dbd79d9c
ty-evans22 <114228074+ty-evans22@users.noreply.github.com>
2023-05-02 00:01:14 -0500

    added delete functionality

--- frontend/html_ass3/src/pages/Delete.js
+++ frontend/html_ass3/src/pages/Delete.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './styles/delete.css';
 import { useState } from 'react';
+import { Button, Table } from 'react-bootstrap';
 
 const Delete = () => {
     const [inputs, setInputs] = useState({});
-    var input;
-    const [products, setProducts] = useState([]);
+    const [product, setProduct] = useState([]);
+    const [isProduct, setIsProduct] = useState(false);
+    const [isValid, setIsValid] = useState(true);
 
     const handleChange = (event) => {
+        setIsValid(true);
         const name = event.target.name;
         const value = event.target.value;
         setInputs(values => ({...values, [name]: value}))
-        input = event.target.value;
     }
 
     const handleSubmit = (event) => {
@@ -21,13 +23,30 @@ const Delete = () => {
     }
 
     const handleGet = async () => {
-        const response = await fetch('/products/' + {input});
-        const body = await response.json();
-        if (response.status !== 200) throw Error(body.message);
+        try {
+            const response = await fetch('/products/' + inputs.id);
+            const body = await response.json();
+            if (response.status !== 200) throw Error(body.message);
+
+            console.log(body);
+            setProduct(body);
+            setIsProduct(true);
+        } catch {
+            setIsValid(false);
+            setIsProduct(false);
+        }
+    };
+
+    const handleDelete = async e => {
+        e.preventDefault();
+        const response = await fetch('/products/' + inputs.id, {
+            method: 'DELETE'
+        });
+        const body = await response.text();
 
         console.log(body);
-        setProducts(body);
-    };
+        setIsProduct(false);
+    }
 
     return (
         <div className='delete-page'>
@@ -38,6 +57,40 @@ const Delete = () => {
                 </label>
                 <input type='submit' />
             </form>
+            {!isValid &&
+                <p>The given id is does not exist in the database.</p>
+            }
+            {isProduct &&
+                <div className='delete-prompt'>
+                    <Table responsive bordered>
+                        <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>Title</th>
+                                <th>Price</th>
+                                <th>Description</th>
+                                <th>Category</th>
+                                <th>Image</th>
+                                <th>Rating Rate</th>
+                                <th>Rating Count</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{product.id}</td>
+                                <td>{product.title}</td>
+                                <td>{product.price}</td>
+                                <td>{product.description}</td>
+                                <td>{product.category}</td>
+                                <td><img src={product.image} alt={product.image} /></td>
+                                <td>{product.rating.rate}</td>
+                                <td>{product.rating.count}</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                    <Button onClick={handleDelete}>Delete Product</Button>
+                </div>
+            }
         </div>
     );
 };
