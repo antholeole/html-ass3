568d800d56da1cd29c0b0fc3eef536eb6d823896
ty-evans22 <114228074+ty-evans22@users.noreply.github.com>
2023-05-04 19:01:52 -0500

    started updating navbar to bootstrap

--- frontend/src/components/navbar/NavBar.tsx
+++ frontend/src/components/navbar/NavBar.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, NavbarBrand } from 'react-bootstrap';
 import { Pages } from "../../contexts/Page";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 import './NavBar.css';
 
 export const NavBar = () => {
     const pages = Pages.useContainer();
 
-    return <>
-        <nav className='navigation'>
-            <div className='navigation-menu'>
-                <ul>
-                    <li>
-                        <Button className='page-link' onClick={ pages.goToLanding } >Home</Button>
-                    </li>
-                    <li>
-                        <Button className='page-link' onClick={ pages.goToProducts } >Products</Button>
-                    </li>
-                    <li>
-                        <Button className='page-link' onClick={ pages.goToCart } >Cart</Button>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </>
+    return (
+        <Navbar bg='light'>
+            <Container>
+                <Navbar.Brand onClick={ pages.goToLanding }>IRL Fire Reaction Clothing Co.</Navbar.Brand>
+                <Nav>
+                    <Nav.Link onClick={ pages.goToProducts } >Products</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+
+    // return <>
+    //     <nav className='navigation'>
+    //         <div className='navigation-menu'>
+    //             <ul>
+    //                 <li>
+    //                     <Button className='page-link' onClick={ pages.goToLanding } >Home</Button>
+    //                 </li>
+    //                 <li>
+    //                     <Button className='page-link' onClick={ pages.goToProducts } >Products</Button>
+    //                 </li>
+    //                 <li>
+    //                     <Button className='page-link' onClick={ pages.goToCart } >Cart</Button>
+    //                 </li>
+    //             </ul>
+    //         </div>
+    //     </nav>
+    // </>
 }
